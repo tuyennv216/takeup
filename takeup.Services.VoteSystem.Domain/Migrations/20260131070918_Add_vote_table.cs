cda72677fead8750bfe9ee1abe1cd9682e93a46f
Văn Tuyến <nvt.m82a1@outlook.com>
2026-02-07 14:59:41 +0700

    add table

---
+++ takeup.Services.VoteSystem.Domain/Migrations/20260131070918_Add_vote_table.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace takeup.Services.VoteSystem.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_vote_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vote",
+                columns: table => new
+                {
+                    VoteId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IPHash = table.Column<Guid>(type: "TEXT", nullable: false),
+                    TopicId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DataId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ExpiredAt = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vote", x => x.VoteId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vote");
+        }
+    }
+}
